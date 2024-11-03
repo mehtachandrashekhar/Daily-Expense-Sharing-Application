9484ca90f0e408e7b310cca5011f802cc7d3a863
mehtachandrashekhar <mehtachandrashekhar84@gmail.com>
2024-11-03 15:08:42 +0530

    initial commit

---
+++ expenses/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+
+# Create your models here.
+
+
+class User(models.Model):
+    email = models.EmailField(unique=True)
+    name = models.CharField(max_length=100)
+    mobile_number = models.CharField(max_length=15)
+
+
+class Expense(models.Model):
+    objects = None
+    METHODS = [('equal', 'Equal'), ('exact', 'Exact'), ('percentage', 'Percentage')]
+
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    method = models.CharField(max_length=10, choices=METHODS)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    participants = models.ManyToManyField(User, related_name='expenses')
+    exact_amounts = models.JSONField(blank=True, null=True)
+    percentage_amounts = models.JSONField(blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.user.name} - {self.amount} - {self.method}"
